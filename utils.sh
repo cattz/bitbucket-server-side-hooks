c6aabcbac4ce472bcca5b2c9b41916ce90ca06ee
Xabier Davila <davila.xabier@gmail.com>
2018-05-11 13:52:07 +0200

    Improve logging

--- utils.sh
+++ utils.sh
@@ -2,27 +2,52 @@
 
 # Basic util functions
 
+
 #timestamp
 DATE=$(date '+%Y/%m/%d:%H:%M:%S')
 # log folder
 LOG_FOLDER="${DIR}/log"
 
-mkdir -p ${LOG_FOLDER}
+mkdir -p "${LOG_FOLDER}"
 
-function log_base {
-
-	log_file=${1}
-	level=${2}
-	msg=${3}
-	echo "${DATE} ${level} - ${msg}" >> ${LOG_FOLDER}/hook_${log_file}
+log_to_num() {
+	case ${1} in
+		ERROR ) echo 40 ;;
+	    WARNING ) echo 30 ;;
+		DEBUG ) echo 10 ;;
+	    * ) echo 20 ;;
+	esac
 }
 
-function log_debug {
-	if [ ${LOGLEVEL} == "DEBUG" ]; then
-		log_base ${1} "[ DEBUG ]" "${2}"
+
+log() {
+
+	local level=${1}   # Deliberatly removed quotes to trim the level string
+	local msg="${2}"
+	local log_file=${3:-general.log}
+
+
+	log_set_level=$(log_to_num ${LOGLEVEL})
+	log_print_level=$(log_to_num ${level})
+
+	if [ "${log_print_level}" -ge "${log_set_level}" ]; then
+		echo "${DATE} [${level}] - ${msg}" | tee -a ${LOG_FOLDER}/hook_${log_file}
 	fi
 }
 
-function log_info {
-	log_base ${1} "[ INFO  ]" "${2}"
+
+log_debug() { 
+	log "DEBUG  " "${1}" "${2}"
+}
+
+log_info() { 
+	log "INFO   " "${1}" "${2}"
+}
+
+log_warning() {
+	log "WARNING" "${1}"m "${2}"
+}
+
+log_error() {
+	log "ERROR  " "${1}" "${2}"
 }
